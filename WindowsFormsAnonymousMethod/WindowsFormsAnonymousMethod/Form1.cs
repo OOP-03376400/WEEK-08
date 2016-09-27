81716a36a2f6fa045bb4a381d5ce81ba63779537
Koson Trachu <koson.trachu@gmail.com>
2016-09-27 20:22:37 +0700

    1

---
+++ WindowsFormsAnonymousMethod/WindowsFormsAnonymousMethod/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAnonymousMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        delegate int OtherDel(int InParam);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OtherDel del = delegate (int x)
+            {
+                return x + 20;
+            };
+            textBox1.Text = del(5).ToString();
+            textBox2.Text = del(6).ToString();
+        }
+    }
+}
