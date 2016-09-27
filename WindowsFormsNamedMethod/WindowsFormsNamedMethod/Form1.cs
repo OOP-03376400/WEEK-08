95ab22621d8bc26cda6d54ae8b6208194e17da19
Koson Trachu <koson.trachu@gmail.com>
2016-09-27 20:15:30 +0700

    1

---
+++ WindowsFormsNamedMethod/WindowsFormsNamedMethod/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsNamedMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int Add20(int x)
+        {
+            return x + 20;
+        }
+
+        delegate int OtherDel(int InParam);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OtherDel del = Add20;
+
+            textBox1.Text = del(5).ToString();
+            textBox2.Text = del(6).ToString();
+        }
+    }
+}
